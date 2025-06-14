279cf29cfa24bbd38e52e85df6d93602f3049bcf
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2025-06-14 15:32:06 +0530

    Added delete property

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,12 @@ export default function App() {
               })
           })
   }
+  function deleteTodo (id)
+  {
+       setTodos(currentTodos =>{
+          return currentTodos.filter(todo => todo.id !== id )
+       })
+  }
   return (
     <>
       <div className="container">
@@ -46,6 +52,7 @@ export default function App() {
         <h1 className="Header">Todo</h1>
 
         <ul className="list">
+          {todos.length === 0 && "No Todos"}
           {todos.map((todo) => {
             return (
               <li key = {todo.id}>
@@ -54,7 +61,7 @@ export default function App() {
                   onChange={e => {toggleTodo(todo.id , e.target.checked)}} />
                   {todo.title}
                 </label>
-                <button className="btn btn-danger">Delete</button>
+                <button   onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
               </li>
             );
           })}
